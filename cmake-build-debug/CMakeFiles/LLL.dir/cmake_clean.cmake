b5f958fba78b7e7dcf358ecd27b500570e526fa4
royshurek <Roy.Shurek@gmail.com>
2019-08-08 21:06:40 +0300

    Additional matrix operations and tests :construction:

---
+++ cmake-build-debug/CMakeFiles/LLL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLL.dir/lll.cpp.o"
+  "LLL.pdb"
+  "LLL.exe"
+  "libLLL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/User/CLionProjects/LLL/cmake-build-debug/googletest-src/googletest
+# Build directory: /cygdrive/c/Users/User/CLionProjects/LLL/cmake-build-debug/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
