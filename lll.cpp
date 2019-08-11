11b4b676e88ef58eb12a257ec58b80d9dda9b5ff
royshurek <Roy.Shurek@gmail.com>
2019-08-11 20:01:22 +0300

    - Small fix in lll's implementation

--- lll.cpp
+++ lll.cpp
@@ -5,15 +5,15 @@
 // Is given a (usually linearly independent) basis on which it computes gram - schmidt, and a matrix in which to write the computed coefficients
 std::vector<Vector<mpq_class>> update(unsigned int size, Matrix<mpq_class>& m, const std::vector<Vector<mpq_class>>& basis)
 {
-    std::vector<Vector<mpq_class>> ortho = gs(basis);
+    std::vector<Vector<mpq_class>> ortho_basis = gs(basis);
 
-    for (unsigned int i = 0; i < size; i++) {
-        for (unsigned int j = 0; j < size; j++) {
-            m(i, j) = (basis[i] * ortho[j]) / (ortho[j] * ortho[j]);
+    for (unsigned int i = 0; i < size; ++i) {
+        for (unsigned int j = 0; j < size; ++j) {
+            m(i, j) = (basis[i] * ortho_basis[j]) / (ortho_basis[j] * ortho_basis[j]);
         }
     }
 
-    return ortho;
+    return ortho_basis;
 }
 
 // The naive implementation of the LLL algorithm, as can be found in the internet
@@ -22,36 +22,36 @@ std::vector<Vector<mpq_class>> lll(double delta, const std::vector<Vector<mpq_cl
 {
     Vector<mpq_class> temp;
 
-    std::vector<Vector<mpq_class>> our_basis = basis;
+    std::vector<Vector<mpq_class>> reduced_basis = basis;
     unsigned int size = basis.size();
 
     Matrix<mpq_class> m(size, size);
-    std::vector<Vector<mpq_class>> ortho = update(size, m, our_basis);
+    std::vector<Vector<mpq_class>> ortho_basis = update(size, m, reduced_basis);
 
     unsigned int k = 1;
     while (k <= size - 1) {
         for (int j = k - 1; j >= 0; --j) {
             if (abs(m(k, j)) > 0.5) {
-                our_basis[k] -= (mpq_class)floor((mpf_class)(0.5 + m(k, j))) * our_basis[j];
-                ortho = update(size, m, our_basis);
+                reduced_basis[k] -= (mpq_class)floor((mpf_class)(0.5 + m(k, j))) * reduced_basis[j];
+                ortho_basis = update(size, m, reduced_basis);
             }
         }
-        if (ortho[k] * ortho[k] >= (delta - m(k, k - 1) * m(k, k - 1)) * (ortho[k - 1] * ortho[k - 1])) {
-            k++;
+        if (ortho_basis[k] * ortho_basis[k] >= (delta - m(k, k - 1) * m(k, k - 1)) * (ortho_basis[k - 1] * ortho_basis[k - 1])) {
+            ++k;
         }
         else {
-            temp = our_basis[k];
-            our_basis[k] = our_basis[k - 1];
-            our_basis[k - 1] = temp;
+            temp = reduced_basis[k];
+            reduced_basis[k] = reduced_basis[k - 1];
+            reduced_basis[k - 1] = temp;
 
-            ortho = update(size, m, our_basis);
+            ortho_basis = update(size, m, reduced_basis);
 
             if (k - 1 > 1)
-                k--;
+                --k;
             else
                 k = 1;
         }
     }
 
-    return our_basis;
+    return reduced_basis;
 }
